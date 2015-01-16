8cef334bd5202781e6309cb460d89ef2b604d0fa
Ismael Cabañas García <pajarokillo@gmail.com>
2015-01-16 22:27:39 +0100

    Refactorización

--- orienteering-parent/orienteering-dtos/src/main/java/cabanas/garcia/orienteering/dtos/club/ClubBusquedaForm.java
+++ orienteering-parent/orienteering-dtos/src/main/java/cabanas/garcia/orienteering/dtos/club/ClubBusquedaForm.java
@@ -2,6 +2,11 @@ package cabanas.garcia.orienteering.dtos.club;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+import cabanas.garcia.orienteering.dtos.club.ClubBusquedaForm.Builder;
+
 /**
  * Clase que representa los datos del formulario de búsqueda de un club de orientación.
  * 
@@ -24,9 +29,35 @@ public class ClubBusquedaForm implements Serializable {
 			return dto;
 		}
 
+
+		public Builder conNombre(String unNombreDeClub) {
+			dto.setNombre(unNombreDeClub);
+			return this;
+		}
+
 	}
 
 	private static final long serialVersionUID = 1L;
+
+	public static Builder getBuilder() {
+		return new Builder();
+		
+	}
+
+	private String nombre;
+
+	public void setNombre(String unNombreDeClub) {
+		this.nombre = unNombreDeClub;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+	}
 
 	
 }
